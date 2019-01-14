49132a76698f694571e0215f6c2fee61565642ed
Tyron S <35595467+TyronS98@users.noreply.github.com>
2019-01-14 15:52:47 +0100

    Add files via upload

---
+++ Banksystem/Bank.cpp
@@ -0,0 +1,63 @@
+#include <string>
+#include <iostream>
+
+#include "Bank.h"
+
+cBank g_Bank;
+
+void cAutomat::GeldkarteInput() {
+	std::cout << "\nBenutzername der Karte eingeben: ";
+	std::cin >> szBenutzername;
+};
+
+bool cAutomat::PinEingeben() {
+	std::cout << "\nGeben Sie ihre Geheimzahl ein: ";
+	std::cin >> szPIN;
+	return g_Bank.Zugriffsberechtigung(szBenutzername,szPIN);
+};
+
+bool cAutomat::BetragEingabe() {
+	int iBetrag, iSelection;
+	std::cout << "\nMoechten Sie Geld abheben oder einzahlen?\n\nAbheben = 0\nEinzahlen = 1\n\n";
+	std::cin >> iSelection;
+
+	const auto bWithdraw = iSelection == 0;
+
+	std::cout << "\nGeben Sie ihren gewuenschten Betrag ein: ";
+	std::cin >> iBetrag;
+	return g_Bank.MoneyDraw(iBetrag, bWithdraw);
+};
+
+
+
+void cBank::Init(std::string p_szBenutzername, std::string p_szPIN, int p_iKontostand) {
+	szBenutzername = p_szBenutzername;
+	szPIN = p_szPIN;
+	iKontostand = p_iKontostand;
+};
+
+bool cBank::Zugriffsberechtigung(std::string p_szBenutzername, std::string p_szPIN) {
+	if (p_szBenutzername == szBenutzername && p_szPIN == szPIN) {
+		return true;
+	}
+	return false;
+};
+
+bool cBank::MoneyDraw(int p_iBetrag, bool p_Bwithdraw) {
+	if (iKontostand >= p_iBetrag && p_Bwithdraw) {
+		iKontostand -= p_iBetrag;
+		return true;
+	}
+	else if(!p_Bwithdraw){
+		iKontostand += p_iBetrag;
+		return true;
+	}
+	else {
+		std::cout << "\nIhr Kontostand ist niedriger als ihre gewuenschte Summe\nKontostand: " << iKontostand;
+		return false;
+	}
+};
+
+void cBank::GetKontostand() {
+	std::cout <<iKontostand;
+};

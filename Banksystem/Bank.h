3a6b62aa621c98e92e5e6db2a73a26d7b553455e
Tyron S <35595467+TyronS98@users.noreply.github.com>
2019-01-15 20:17:06 +0100

    Update Bank.h

--- Banksystem/Bank.h
+++ Banksystem/Bank.h
@@ -3,7 +3,7 @@
 
 class cBank {
 public:
-	bool MoneyDraw(int p_iBetrag, bool withdraw);
+	bool MoneyDraw(int p_iBetrag, bool p_iWithdraw);
 	bool Zugriffsberechtigung(std::string p_szBenutzername, std::string p_szPIN);
 	void Init(std::string p_szBenutzername, std::string p_szPIN, int p_iKontostand);
 	void GetKontostand();
